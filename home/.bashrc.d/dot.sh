4bcbb5592659b13cf7bd6eda6a10563ce313c048
adyang <20383270+adyang@users.noreply.github.com>
2019-10-20 01:56:06 +0800

    Add dot command to configure macOS.

--- home/.bashrc.d/dot.sh
+++ home/.bashrc.d/dot.sh
@@ -3,10 +3,14 @@
 # shellcheck source=/dev/null
 source "${HOME}/.dot-env"
 
+dot::install() {
+  "${DOT_DIR}"/install
+}
+
 dot::vim() {
   "${DOT_DIR}"/configure-vim
 }
 
-dot::install() {
-  "${DOT_DIR}"/install
+dot::macos() {
+  "${DOT_DIR}"/configure-macos
 }
