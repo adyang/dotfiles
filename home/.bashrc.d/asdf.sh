74617e4e93b6de1a810193b21b5e5c6970aa2485
adyang <20383270+adyang@users.noreply.github.com>
2019-10-24 22:42:30 +0800

    Add function to change JAVA_HOME when asdf java version changes.

--- home/.bashrc.d/asdf.sh
+++ home/.bashrc.d/asdf.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 source /usr/local/opt/asdf/asdf.sh
 
-if asdf current java >/dev/null 2>&1; then
-  asdf_java_path="$(asdf which java)"
-  export JAVA_HOME="${asdf_java_path%/*/*}"
-  unset -v asdf_java_path
-fi
+asdf_java_home() {
+  local asdf_java_path
+  if asdf current java >/dev/null 2>&1; then
+    asdf_java_path="$(asdf which java)" || return "$?"
+    export JAVA_HOME="${asdf_java_path%/*/*}"
+  fi
+}
+
+asdf_java_home
