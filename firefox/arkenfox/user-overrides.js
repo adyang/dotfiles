8a9cd593dd19b8970173ded2ddfd8bfaf32a758e
adyang <20383270+adyang@users.noreply.github.com>
2022-04-21 22:02:56 +0800

    Configure firefox via arkenfox.

---
+++ firefox/arkenfox/user-overrides.js
@@ -0,0 +1,7 @@
+user_pref("extensions.pocket.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("network.http.referer.XOriginPolicy", 1);
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
+
+user_pref("keyword.enabled", true);
