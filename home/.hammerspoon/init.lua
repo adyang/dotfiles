0b2fd82eb719959345b92516dfa01225e79ae45c
adyang <20383270+adyang@users.noreply.github.com>
2023-07-02 21:47:53 +0800

    Fix duplicate hotkeys used for window management.

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -10,35 +10,32 @@ grid.setMargins({0, 0})
 
 hotkey.bind(cmdAltCtrl, "M", grid.maximizeWindow)
 
-hotkey.bind(cmdAltCtrl, ";", function()
-    grid.set(window.focusedWindow(), {x=0,y=0,w=2,h=4})
-end)
-hotkey.bind(cmdAltCtrl, "'", function()
-    grid.set(window.focusedWindow(), {x=2,y=0,w=2,h=4})
-end)
-hotkey.bind(cmdAltCtrl, "I", function()
-    grid.set(window.focusedWindow(), {x=0,y=0,w=4,h=2})
-end)
-hotkey.bind(cmdAltCtrl, "M", function()
-    grid.set(window.focusedWindow(), {x=0,y=2,w=4,h=2})
-end)
-hotkey.bind(cmdAltCtrl, "Y", function()
-    grid.set(window.focusedWindow(), {x=0,y=0,w=2,h=2})
-end)
-hotkey.bind(cmdAltCtrl, "U", function()
-    grid.set(window.focusedWindow(), {x=2,y=0,w=2,h=2})
-end)
-hotkey.bind(cmdAltCtrl, "B", function()
-    grid.set(window.focusedWindow(), {x=0,y=2,w=2,h=2})
-end)
-hotkey.bind(cmdAltCtrl, "N", function()
-    grid.set(window.focusedWindow(), {x=2,y=2,w=2,h=2})
-end)
+local function tileTo(cell)
+    return function() grid.set(window.focusedWindow(), cell) end
+end
+local cells = {
+    left = {x=0,y=0,w=2,h=4},
+    right = {x=2,y=0,w=2,h=4},
+    top = {x=0,y=0,w=4,h=2},
+    bottom = {x=0,y=2,w=4,h=2},
+    topLeft = {x=0,y=0,w=2,h=2},
+    topRight = {x=2,y=0,w=2,h=2},
+    bottomLeft = {x=0,y=2,w=2,h=2},
+    bottomRight = {x=2,y=2,w=2,h=2}
+}
+hotkey.bind(cmdAltCtrl, ";", tileTo(cells.left))
+hotkey.bind(cmdAltCtrl, "'", tileTo(cells.right))
+hotkey.bind(cmdAltCtrl, "P", tileTo(cells.top))
+hotkey.bind(cmdAltCtrl, "/", tileTo(cells.bottom))
+hotkey.bind(cmdAltCtrl, "Y", tileTo(cells.topLeft))
+hotkey.bind(cmdAltCtrl, "U", tileTo(cells.topRight))
+hotkey.bind(cmdAltCtrl, "B", tileTo(cells.bottomLeft))
+hotkey.bind(cmdAltCtrl, "N", tileTo(cells.bottomRight))
 
 hotkey.bind(cmdAltCtrl, ".", grid.resizeWindowWider)
 hotkey.bind(cmdAltCtrl, ",", grid.resizeWindowThinner)
-hotkey.bind(cmdAltCtrl, "P", grid.resizeWindowTaller)
-hotkey.bind(cmdAltCtrl, "O", grid.resizeWindowShorter)
+hotkey.bind(cmdAltCtrl, "O", grid.resizeWindowTaller)
+hotkey.bind(cmdAltCtrl, "I", grid.resizeWindowShorter)
 
 hotkey.bind(cmdAltCtrl, "J", grid.pushWindowDown)
 hotkey.bind(cmdAltCtrl, "K", grid.pushWindowUp)
