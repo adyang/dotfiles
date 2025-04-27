483f70832d65557dde9deca5fc2bd2f30f4ac24a
adyang <20383270+adyang@users.noreply.github.com>
2025-04-27 18:18:26 +0800

    Remove bash related configurations.

--- home/dot_bashrc.d/0-homebrew.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [[ "$(sysctl -n machdep.cpu.brand_string)" =~ 'Apple' ]]; then
-  export PATH="/opt/homebrew/bin:${PATH}"
-fi
--- home/dot_bashrc.d/dotnet.sh
+++
@@ -1,2 +0,0 @@
-export DOTNET_CLI_TELEMETRY_OPTOUT='true'
-
